596f103e7a48c6b567b7cbae17bfb8441c787502
satriatf <satriatf11@gmail.com>
2024-06-10 21:42:56 +0700

    TUGAS 13

--- resources/views/admin/partials/_header.blade.php
+++ resources/views/admin/partials/_header.blade.php
@@ -175,7 +175,7 @@
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
               <h6>{{ Auth::user()->name }}</h6>
-              <span>Programmer</span>
+              <span>{{ Auth::user()->email }}</span>
             </li>
             <li>
               <hr class="dropdown-divider">
